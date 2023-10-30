3f230d81f553e752fb47f54d84e701114d588027
Mr.Twister <sardo.ip@sardo.work>
2023-10-31 01:07:48 +0800

    Replace bootstrap.min.css from cdn to local

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -2,6 +2,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>FEP机器人</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/lux/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
     <link href="/css/app.css" rel="stylesheet">
 </head>
